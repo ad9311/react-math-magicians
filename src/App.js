a7c5cfafd2b7508172060b393c748a2ec6a759f0
Ángel Díaz <diazangel9311@gmail.com>
2021-09-14 19:40:46 -0500

    Convert app from func to class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import Calculator from './components/Calculator';
-import './app.css';
+import './App.css';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
