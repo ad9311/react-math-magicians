6be8a9e79bfa57b8950b476e2784a8fe6c64e66d
Ángel Díaz <diazangel9311@gmail.com>
2021-09-16 20:19:53 -0500

    Refactor Calculator.js with hooks
    Convert class components back to functional components.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Calculator />
+  </div>
+);
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,92 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import './calculator.css';
 import Key from './Key';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.buffer = '';
-    this.state = { total: null, next: null };
-  }
+const Calculator = () => {
+  const [data, setData] = useState({ total: null, next: null, operation: null });
 
-  handleKeyDown = (keyPressed) => {
-    this.setState((obj) => calculate(obj, keyPressed));
-  }
+  const handleClick = (keyPressed) => {
+    setData(() => calculate(data, keyPressed));
+  };
 
-  returnKey = (value, index = 0, array = []) => {
+  const returnKey = (value, index = 0, array = []) => {
     if (index === (array.length - 1)) {
       return (
-        <Key key={value} value={value} orange handleKeyDown={this.handleKeyDown} />
+        <Key key={value} value={value} orange event={handleClick} />
       );
     }
     return (
-      <Key key={value} value={value} handleKeyDown={this.handleKeyDown} />
+      <Key key={value} value={value} event={handleClick} />
     );
-  }
+  };
 
-  returnDiv = (row1, row2 = '') => (
+  const returnDiv = (row1, row2 = '') => (
     <div className="row">
       {row1}
       {row2}
     </div>
   );
 
-  render() {
-    const { next, total, operation } = this.state;
-    const firstRowArr = ['AC', '+/-', '%', '÷'];
-    const secondRowArr = ['7', '8', '9', 'x'];
-    const thirdRowArr = ['4', '5', '6', '-'];
-    const fourthRowArr = ['1', '2', '3', '+'];
-    const dotEqualArr = ['.', '='];
+  const firstRowArr = ['AC', '+/-', '%', '÷'];
+  const secondRowArr = ['7', '8', '9', 'x'];
+  const thirdRowArr = ['4', '5', '6', '-'];
+  const fourthRowArr = ['1', '2', '3', '+'];
+  const dotEqualArr = ['.', '='];
 
-    const firstRow = firstRowArr.map(
-      (value, index) => (
-        this.returnKey(value, index, firstRowArr)
-      ),
-    );
+  const firstRow = firstRowArr.map(
+    (value, index) => (
+      returnKey(value, index, firstRowArr)
+    ),
+  );
 
-    const secondRow = secondRowArr.map(
-      (value, index) => (
-        this.returnKey(value, index, secondRowArr)
-      ),
-    );
+  const secondRow = secondRowArr.map(
+    (value, index) => (
+      returnKey(value, index, secondRowArr)
+    ),
+  );
 
-    const thirdRow = thirdRowArr.map(
-      (value, index) => (
-        this.returnKey(value, index, thirdRowArr)
-      ),
-    );
+  const thirdRow = thirdRowArr.map(
+    (value, index) => (
+      returnKey(value, index, thirdRowArr)
+    ),
+  );
 
-    const fourthRow = fourthRowArr.map(
-      (value, index) => (
-        this.returnKey(value, index, fourthRowArr)
-      ),
-    );
+  const fourthRow = fourthRowArr.map(
+    (value, index) => (
+      returnKey(value, index, fourthRowArr)
+    ),
+  );
 
-    const dotEqual = dotEqualArr.map(
-      (value, index) => (
-        this.returnKey(value, index, dotEqualArr)
-      ),
-    );
+  const dotEqual = dotEqualArr.map(
+    (value, index) => (
+      returnKey(value, index, dotEqualArr)
+    ),
+  );
 
-    return (
-      <div className="keypad">
-        <div className="io-container">
-          <output className="d-block text-right pr-1">{next || operation || total || '0'}</output>
-        </div>
-        {this.returnDiv(firstRow)}
-        {this.returnDiv(secondRow)}
-        {this.returnDiv(thirdRow)}
-        {this.returnDiv(fourthRow)}
-        {this.returnDiv(
-          this.returnDiv(this.returnKey('0')),
-          this.returnDiv(dotEqual),
-        )}
+  return (
+    <div className="keypad">
+      <div className="io-container">
+        <output className="d-block text-right pr-1">
+          { data.next || data.operation || data.total || '0'}
+        </output>
       </div>
-    );
-  }
-}
+      {returnDiv(firstRow)}
+      {returnDiv(secondRow)}
+      {returnDiv(thirdRow)}
+      {returnDiv(fourthRow)}
+      {returnDiv(
+        returnDiv(returnKey('0')),
+        returnDiv(dotEqual),
+      )}
+    </div>
+  );
+};
 
 export default Calculator;
