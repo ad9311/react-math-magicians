910c5e73b9c9752d81cf02733eb35b2c5fc81459
Ángel Díaz <diazangel9311@gmail.com>
2021-09-22 15:21:58 -0500

    Add tests for Key component

---
+++ src/__tests__/Calculator.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {render, fireEvent, screen} from '@testing-library/react';
+import Calculator from '../components/Calculator';
+
+describe('Calculator component', () => {
+  test('5 x 3', () => {
+    render(<Calculator />);
+
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('x'));
+    fireEvent.click(screen.getByText('3'));
+    fireEvent.click(screen.getByText('='));
+
+    expect(screen.getByText('15')).toMatchSnapshot();
+  });
+
+  test('15 + 5', () => {
+    render(<Calculator />);
+
+    fireEvent.click(screen.getByText('1'));
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('+'));
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('='));
+
+    expect(screen.getByText('20')).toMatchSnapshot();
+  });
+});
